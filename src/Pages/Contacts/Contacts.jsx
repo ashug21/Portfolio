041babd76231a33ea620f4a7799d0bcaa7f87ec6
Ashmeet Kalsi <ashmeetkalsi@Ashmeets-MacBook-Air.local>
2025-10-16 23:34:04 +0530

    Initial commit: add portfolio project

---
+++ src/Pages/Contacts/Contacts.jsx
@@ -0,0 +1,55 @@
+import React, { useRef,useEffect } from 'react';
+
+import "./Contacts.css";
+import Navbar from "../../Components/Navbar/Navbar";
+import { ToastContainer, toast } from 'react-toastify';
+import { useLocation } from "react-router-dom";
+
+const Contact123 = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+ 
+  const notify = () => toast("SuccessFully Submitted");
+
+ 
+
+  return (
+    <div className="contact-page123">
+      <Navbar/>
+      <ToastContainer
+position="top-right"
+autoClose={4000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="dark"
+
+/>
+      <h1 id="heading123">Contact Me</h1>
+
+      <br/>
+
+      <div className="contact-container123">
+        {/* Contact Form */}
+       
+        {/* Contact Info */}
+        <div className="contact-info123">
+          <h2 className="heading2123">Get in touch</h2>
+          <p>Email: ashmeetsinghkalsi.dev@gmail.com</p>
+          <p>Phone: +91-9971911118</p>
+          <p>Location: India</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact123;
---
+++ src/Pages/Projects/Projects.jsx
@@ -0,0 +1,86 @@
+import React,{useEffect} from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import './Projects.css'
+import { Link } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+
+const Projects = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <div>
+      <Navbar/>
+      <br/><br/>
+      <div className='project-container'>
+      <h1>Project-1 : CryptoPlace</h1>
+      <br/>
+      <p> <strong>Description : </strong> CryptoPlace is a React-based web application that provides real-time cryptocurrency details, including price, market cap, and symbol for popular coins. It fetches live data from APIs and displays it in a clean, responsive interface. The project helps users easily track crypto market trends and stay updated with accurate, up-to-date financial information.</p>
+      <br/>
+      <p><strong>Languages Used : </strong> React, Firebase</p>
+      <br/>
+      <div className='link-div'>
+      <Link className='link-1' to="https://crypto-place-alpha-rouge.vercel.app">Project URL</Link>
+      <Link className='link-2' to="https://github.com/ashug21/CryptoPlace.git">Github Link</Link>
+      </div>
+      
+      </div>
+
+      <div className='project-container'>
+      <h1>Project-2 : WebTV</h1>
+      <br/>
+      <p> <strong>Description : </strong>WebTV is a full-stack web application that works like an OTT platform. Users can sign up and log in to access the app, browse, and watch movie trailers. It also has a subscription page (still under development) and offers other features to make the platform more interactive and user-friendly. WebTV aims to provide a smooth streaming experience while continuously adding new functionalities for a better user experience.</p>
+      <br/>
+      <p><strong>Languages Used : </strong> Ejs, NodeJs , MonogoDb Atlas , Ejs, Javascript </p>
+      <br/>
+      <div className='link-div'>
+      <Link className='link-1' to="https://web-tv-ten.vercel.app">Project URL</Link>
+      <Link className='link-2' to="https://github.com/ashug21/WebTV.git">Github Link</Link>
+      </div>
+      
+      </div>
+
+      <div className='project-container'>
+      <h1>Project-3 : Blinkit Clone</h1>
+      <br/>
+      <p> <strong>Description : </strong> This Project is a clone for popular grocery delivery web application, built using React and Firebase. The platform replicates the core functionality and sleek user interface of Blinkit, allowing users to browse grocery items, add products to their cart. Firebase handles authentication, real-time database operations, and cloud hosting, ensuring smooth performance and secure data management. The app is fully responsive, providing an optimized experience across mobile and desktop devices. This project demonstrates front-end development skills, state management, and seamless integration with cloud-based backend services to deliver a fast, reliable.</p>
+      <br/>
+      <p><strong>Languages Used : </strong> React, Firebase</p>
+      <br/>
+      <div className='link-div'>
+      <Link className='link-1' to="https://blinkit-clone-gray.vercel.app">Project URL</Link>
+      <Link className='link-2' to="https://github.com/ashug21/BlinkitClone.git">Github Link</Link>
+      </div>
+      
+      </div>
+
+      
+
+
+     
+
+      <div className='project-container'>
+      <h1>Project-4 : Weather App</h1>
+      <br/>
+      <p> <strong>Description : </strong> Weather Forecast is a React-based web application that provides real-time weather updates for any city using the WeatherAPI. Users can enter a location to instantly view temperature, humidity, wind speed, and weather conditions in both Celsius and Fahrenheit. The app features a clean, responsive design and uses API integration to deliver accurate, up-to-date weather information, making it simple and efficient for users to check global weather conditions anytime.</p>
+      <br/>
+      <p><strong>Languages Used : </strong> React</p>
+      <br/>
+      <div className='link-div'>
+      <Link className='link-1' to="https://weather-app-jade-nine-70.vercel.app">Project URL</Link>
+      <Link className='link-2' to="https://github.com/ashug21/WeatherApp.git">Github Link</Link>
+      </div>
+      
+      </div>
+
+
+     
+     
+    </div>
+  )
+}
+
+export default Projects
